3a614eedef1e24f622ba790dc5da7b0bc9123ae1
Haider007A <2021384@student.cct.ie>
2023-11-16 23:35:03 +0000

    ReqestFunction

--- TaxiRide.sol
+++ TaxiRide.sol
@@ -4,4 +4,6 @@ pragma solidity ^0.7.5;
 
 contract TaxiRide {
     
+     function requestRide(string memory _destination) external payable {
+        require(msg.value > 0, "Ether value must be greater than 0");
 }

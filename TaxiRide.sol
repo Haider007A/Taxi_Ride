c975ae33c5fcfc110b9a0b18438881385cabc18f
Haider007A <2021384@student.cct.ie>
2023-11-22 23:40:34 +0000

    Add Requested function by creating event of ride

--- TaxiRide.sol
+++ TaxiRide.sol
@@ -4,7 +4,19 @@ pragma solidity ^0.7.5;
 
 contract TaxiRide {
     
-     function requestRide(string memory _destination) external payable {
-        require(msg.value > 0, "Ether value must be greater than 0");
+Struct Ride {
+        address passenger;
+        string destination;
+        uint256 timestamp;
+}
+    event RideRequested(uint256 rideId, address passenger, string destination, uint256 timestamp);
+     function requestRide(string memory _destination) external {
+        rideCount++;
+        Ride storage newRide = rides[rideCount];
+        newRide.passenger = msg.sender;
+        newRide.destination = _destination;
+        newRide.timestamp = block.timestamp;
+
+        emit RideRequested(rideCount, msg.sender, _destination, block.timestamp);
 }
 }
